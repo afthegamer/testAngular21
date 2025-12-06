761f7fcc8ee7073a72481208497303eb4f693c50
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-06 16:07:57 +0100

    Add FaceSnap component and configure Prettier

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaceSnap} from './face-snap/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaceSnap],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('studyAngular');
 }
