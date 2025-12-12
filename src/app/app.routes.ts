1f3c03a0cb91b25d1ddec15112992202f3f9799e
Brad <brad.dossantospatatas@acedigitale.com>
2025-12-12 16:31:29 +0100

    Add LandingPage component with routing integration and initial styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { FaceSnapList } from './face-snap-list/face-snap-list';
+import { LandingPage } from './landing-page/landing-page';
 
-export const routes: Routes = [{ path: 'facesnaps ', component: FaceSnapList }];
+export const routes: Routes = [
+  { path: 'facesnaps', component: FaceSnapList },
+  { path: '', component: LandingPage },
+];
