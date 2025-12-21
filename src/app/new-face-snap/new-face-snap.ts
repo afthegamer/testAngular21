d37df246aaa77c03fe07fa7792eaefc567309c38
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-21 20:37:42 +0100

    Implement NewFaceSnap form: add reactive form with validation, live preview, and improve UI styling

--- src/app/new-face-snap/new-face-snap.ts
+++ src/app/new-face-snap/new-face-snap.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnapModel } from '../model/face-snap.model';
+import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, AsyncPipe, UpperCasePipe, DatePipe],
   templateUrl: './new-face-snap.html',
   styleUrl: './new-face-snap.scss',
 })
-export class NewFaceSnap {
+export class NewFaceSnap implements OnInit {
+  snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnapModel>;
+  formBuilder = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      imageUrl: [null, Validators.required],
+      location: [null],
+    });
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map((formValue) => ({
+        ...formValue,
+        createdAt: new Date(),
+        snaps: 0,
+        id: 0,
+      })),
+    );
+  }
+
+  onSubmitForm() {
+    console.log(this.snapForm.value);
+  }
 }
