325702a5372fc5d01587671badd4d2ba6eb05910
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-22 10:54:47 +0100

    Refactor NewFaceSnap form: inline URL regex and adjust formatting for improved readability

--- src/app/new-face-snap/new-face-snap.ts
+++ src/app/new-face-snap/new-face-snap.ts
@@ -1,5 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { map, Observable } from 'rxjs';
 import { FaceSnapModel } from '../model/face-snap.model';
 import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
@@ -13,17 +19,22 @@ import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 export class NewFaceSnap implements OnInit {
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnapModel>;
-  urlRegex!: RegExp;
   formBuilder = inject(FormBuilder);
 
   ngOnInit(): void {
-    this.urlRegex =
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
     this.snapForm = this.formBuilder.group(
       {
         title: [null, Validators.required],
         description: [null, Validators.required],
-        imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+        imageUrl: [
+          null,
+          [
+            Validators.required,
+            Validators.pattern(
+              /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/,
+            ),
+          ],
+        ],
         location: [null],
       },
       {
