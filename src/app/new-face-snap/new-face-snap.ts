ad2282c10250e3b0d8f767017e91b8cb1f5431cf
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-22 10:39:49 +0100

    Enhance NewFaceSnap form: add URL validation and form-level updateOn blur, disable submit button if form invalid

--- src/app/new-face-snap/new-face-snap.ts
+++ src/app/new-face-snap/new-face-snap.ts
@@ -13,15 +13,23 @@ import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 export class NewFaceSnap implements OnInit {
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnapModel>;
+  urlRegex!: RegExp;
   formBuilder = inject(FormBuilder);
 
   ngOnInit(): void {
-    this.snapForm = this.formBuilder.group({
-      title: [null, Validators.required],
-      description: [null, Validators.required],
-      imageUrl: [null, Validators.required],
-      location: [null],
-    });
+    this.urlRegex =
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+    this.snapForm = this.formBuilder.group(
+      {
+        title: [null, Validators.required],
+        description: [null, Validators.required],
+        imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+        location: [null],
+      },
+      {
+        updateOn: 'blur',
+      },
+    );
     this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
       map((formValue) => ({
         ...formValue,
