d301b2b7b91600075f00bde44a8c75a6aa452496
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2025-12-24 04:50:40 +0100

    Add login UI: design template, styling, and implement authentication logic with AuthService and routing

--- src/app/features/auth/pages/componant/auth/auth.ts
+++ src/app/features/auth/pages/componant/auth/auth.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
 })
-export class Auth {
+export class Auth implements OnInit {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  ngOnInit(): void {}
+
+  onLogin(): void {
+    this.authService.login();
+    this.router.navigateByUrl('/facesnaps');
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
 }
